ed8fce7b13e488cf94e99b1e339ebd39acc1654f
cherinsoas <geevarghesecherin@gmail.com>
2024-11-15 15:46:29 +0530

    projectstructure updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="geminiprojects",
+    version="0.0.1",
+    author="Geevarghese",
+    author_email="varkey123@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain"]
+)
